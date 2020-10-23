a8f938dcde298d7afa8add17598b1997b587d7d2
toneiojimz <ajimeneza77@gmail.com>
2020-10-22 22:36:34 -0700

    created init and str

--- Class Practice/bank_account.py
+++ Class Practice/bank_account.py
@@ -3,6 +3,12 @@
 ##CREATE A BANK CLASS AND METHODTS TO DEPOSIT AND WITHDRAW MONEY FROM THE 
 ##ACCOUNT, IF THE ACCOUNT DOES NOT HAVE ENOUGH FUND, PREVENT WITHDRAWAL
 
-class Bank_Account(self, deposit, balance =0):
+class Bank_Account():
     
-    def __init__
+    #bank account has owner and balance attributes
+    def __init__(self, owner, balance =0):
+        self.owner = owner
+        self.balance = balance
+    
+    def __str__(self):
+        return f'Account owner is: {self.owner}\n Balance : {self.balance}'
