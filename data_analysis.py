f90ab3281a9303c2a963101aec90ed4c5fda1993
toneiojimz <ajimeneza77@gmail.com>
2020-10-23 22:48:08 -0700

    working on importing bug

---
+++ data_analysis.py
@@ -0,0 +1,12 @@
+#this exercise is to practice and learn data sets, understand how they store
+#data, and retrieve it
+
+#dataset included in python 
+ 
+from pydataset import data
+
+df = data('titanic')
+
+print(df)
+
+
