56ff81e02461af5e37df028adbc1b14c80bee7c8
toneiojimz <ajimeneza77@gmail.com>
2020-10-26 10:08:09 -0700

    calculation works

---
+++ Lambda_cs_week1/day_one/calculations.py
@@ -0,0 +1,8 @@
+#create a function that takes length and width and finds the perimiter of rectangle
+
+def find_perimeter(length, width):
+    #logic to calculate the perimeter of a rectangle
+    #formula is 2*length + 2*width
+    return 2*length + 2*width
+
+print(find_perimeter(1,2))
