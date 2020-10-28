4d4e4500c78dd1b32d464f9ee4bc3a2ece344762
toneiojimz <ajimeneza77@gmail.com>
2020-10-28 14:25:37 -0700

    sum two using dictionary solution created

--- time_space_complexity.py
+++ time_space_complexity.py
@@ -11,3 +11,50 @@ Notes:
 """
 def two_sum(nums, target):
     # Your code here
+    # nested for loops/ brute force approach
+    #outer loop loops over elements in nums
+        #inner loop loops over the numbers in front of the outer loop
+    # for i in range(len(nums)):
+    #     for j in range(i+1, len(nums)):
+    #         if nums[i] + nums[j] == target:
+    #             return [i,j]
+
+    #ANOTHER APPROACH TO SOLVE
+    #using a dictionary
+    dict = {} #create empty dictionary
+
+    # loop over the list of nums to populate dictionary
+    for i in range(len(nums)): #O(n)
+        dict[nums[i]] = i #O(1)
+
+    #loop over list of nums
+    for i in range(len(nums)):#O(n)
+
+        #check if target-nums[i] is in dict, and that such number has not been used
+        complement = target - nums[i] #O(1)
+        if complement in dict and dict[complement] != i:
+
+            return [i, dict[target-nums[i]]]
+    
+    return "No solution found" 
+
+
+nums = [0,2,3,4,9]
+target= 2
+
+print(two_sum(nums, target))
+
+
+##################################################################################################
+"""
+Demonstration #2
+Given a non-empty array of integers `nums`, every element appears twice 
+except for one. Write a function that finds the element that only appears once.
+Examples:
+- single_number([3,3,2]) -> 1
+- single_number([5,2,3,2,3]) -> 5
+- single_number([10]) -> 10
+"""
+def single_number(nums):
+    # Your code here
+    pass
