ebac60c9f32be2892066b8d63e6fd37866edd4e0
toneiojimz <ajimeneza77@gmail.com>
2020-11-02 10:07:39 -0800

    adding a 1 to array of ints works

--- Lambda_cs_week2/day one/demo2.py
+++ Lambda_cs_week2/day one/demo2.py
@@ -18,3 +18,30 @@ Explanation: The input array represents the integer 999. 999 + 1 = 1000.
 """
 def plus_one(digits):
     # Your code here
+    #given a list of single digit number that together represent a single number
+    #increment that number by 1
+    #case 1, the right most number is not 9, then increment right most digit by 1
+    #case 2, right most digit is a 9
+    # change 9 to 0, and add 1 to the left digit, if that number is a 9, the also replace
+    #with 0 and add 1 to the left, if all numbers are 9s
+    #we might need to add an integer to the most left position
+
+    #1. work with the input in the format that it comes in(in-place)
+
+    #2 transform the input into integers to more easily work with them
+    #technically case 2 takes constant extra space since numbers are all represented with a fixed size
+
+    #start iterating from right to left
+    for i in range(len(digits)-1, -1, -1):
+        #if we realize that no need to iterate(current digit is not a 9), then break the loop
+        if digits[i] == 9:
+            #change it to 0
+            digits[i] = 0
+        #otherwise, we continue iterating from right to left as long as need to
+        else:
+            #increment the digit
+            digits[i] += 1
+            #since digit is not 9, then break
+            return digits
+
+print(plus_one([1,2,9]))
