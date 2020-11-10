5ae270fde410e454e2cb5fc3625220c898801fae
toneiojimz <ajimeneza77@gmail.com>
2020-11-10 10:07:54 -0800

    breadth first traversal works

--- breadth_first_traversal.py
+++ breadth_first_traversal.py
@@ -20,3 +20,22 @@ class TreeNode:
 
 
     # Your code here
+    def iter_depth_first_traverse(root):
+        if root is None:
+            return []
+
+        result = []
+
+        stack = []
+
+        stack.append(root)
+
+        while len(stack) != 0:
+            node = stack.pop()
+            result.append(node.val)
+
+            if node.right is not None:
+                stack.append(node.right)
+            if node.left is not None:
+                stack.append(node.left)
+        return result
