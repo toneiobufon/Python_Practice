bd1248b6e88efae2aa0b2f8e90c934d2f6638ccd
toneiojimz <ajimeneza77@gmail.com>
2020-11-11 10:37:01 -0800

    updating graph example

--- graphs_example_one.py
+++ graphs_example_one.py
@@ -10,12 +10,40 @@ than the graph's maximum degree.
 *Note: We can color a graph in linear time and space. Also, make sure that your
 solution can handle a loop in a reasonable way.*
 """
+colors = ['Yellow', 'Blue', 'Red', 'Green', 'Purple']
 # Definition for a graph node.
 class GraphNode:
-    def __init__(self, label):
-        self.label = label
+    def __init__(self, val):
+        self.val = val
         self.neighbors = set()
         self.color = None
-
 def color_graph(graph, colors):
     # Your code here
+g1 = GraphNode('G1')
+g2 = GraphNode('G2')
+g3 = GraphNode('G3')
+g4 = GraphNode('G4')
+g5 = GraphNode('G5')
+
+g1.neighbors.add(g2)
+g1.neighbors.add(g4)
+g1.neighbors.add(g3)
+
+g2.neighbors.add(g1)
+g2.neighbors.add(g4)
+g2.neighbors.add(g5)
+
+g3.neighbors.add(g1)
+g3.neighbors.add(g5)
+g3.neighbors.add(g4)
+
+g4.neighbors.add(g1)
+g4.neighbors.add(g2)
+g4.neighbors.add(g3)
+g4.neighbors.add(g5)
+
+g5.neighbors.add(g2)
+g5.neighbors.add(g3)
+g5.neighbors.add(g4)
+
+graph = [g1, g2, g3, g4, g5]
