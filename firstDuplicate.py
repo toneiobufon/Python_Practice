070b05ae3b89bcf0519ef1c9a5792cb77183b64e
toneiojimz <ajimeneza77@gmail.com>
2021-03-05 12:51:46 -0800

    solved first duplicate challenge

--- firstDuplicate.py
+++ firstDuplicate.py
@@ -1,15 +1,31 @@
-##
-Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
 
-Example
+# Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
 
-For a = [2, 1, 3, 5, 3, 2], the output should be firstDuplicate(a) = 3.
+# Example
 
-There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than the second occurrence of 2 does, so the answer is 3.
+# For a = [2, 1, 3, 5, 3, 2], the output should be firstDuplicate(a) = 3.
 
-For a = [2, 2], the output should be firstDuplicate(a) = 2;
+# There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than the second occurrence of 2 does, so the answer is 3.
 
-For a = [2, 4, 3, 5, 1], the output should be firstDuplicate(a) = -1.
+# For a = [2, 2], the output should be firstDuplicate(a) = 2;
+
+# For a = [2, 4, 3, 5, 1], the output should be firstDuplicate(a) = -1.
 
 
-##
+
+
+def firstDuplicate(a):
+
+    new = set()
+
+    for i in range(len(a)):
+
+        if a[i] in new:
+            return a[i]
+        else:
+            new.add(a[i])
+    return -1
+
+a = [2, 1, 3, 5, 3, 2]
+
+print(firstDuplicate(a))
